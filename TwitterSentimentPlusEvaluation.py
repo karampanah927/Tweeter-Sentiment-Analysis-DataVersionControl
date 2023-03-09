6dcd638416d59cca19f50efedfd50927870c4595
karampanah927 <karampanah927@gmail.com>
2023-03-09 08:07:10 +0100

    object oriented

--- TwitterSentimentPlusEvaluation.py
+++ TwitterSentimentPlusEvaluation.py
@@ -2,63 +2,79 @@ import pandas as pd
 from textblob import TextBlob
 import re
 
+class TwitterSentimentAnalyzer:
+    # class variables and methods go here
 
-# preprocess text data
-def preprocess_text(text):
-    # remove URLs
-    text = re.sub(r'http\S+', '', text)
-    # remove mentions
-    text = re.sub(r'@\w+', '', text)
-    # remove hashtags
-    text = re.sub(r'#\w+', '', text)
-    # remove non-alphabetic characters and convert to lowercase
-    text = re.sub(r'[^a-zA-Z ]', '', text).lower()
-    return text
+    def __init__(self):
+        self.df = pd.read_csv('training.csv', usecols=['rawContent'])
+        self.df_test = pd.read_csv('test.csv', usecols=['rawContent'])
+        self.df_testGroundTruth = pd.read_csv('TweetTest.csv')
+        # constructor method
+
+    # preprocess text data
+    def preprocess_text(self,text):
+        # remove URLs
+        text = re.sub(r'http\S+', '', text)
+        # remove mentions
+        text = re.sub(r'@\w+', '', text)
+        # remove hashtags
+        text = re.sub(r'#\w+', '', text)
+        # remove non-alphabetic characters and convert to lowercase
+        text = re.sub(r'[^a-zA-Z ]', '', text).lower()
+        return text
+
+    # perform sentiment analysis
+    def analyze_sentiment(self,text):
+        blob = TextBlob(text)
+        return blob.sentiment.polarity
+
+    def train_Model(self):
+        # read CSV file into a pandas dataframe
 
 
-# perform sentiment analysis
-def analyze_sentiment(text):
-    blob = TextBlob(text)
-    return blob.sentiment.polarity
-def sentiment_result(score):
-    if score>0:
-        result = 'Positive'
-    elif score<0:
-        result = 'Negative'
-    else:
-        result = 'Neutral'
-    return result
+        # print(self.df.head(3))
+        # # df = df.loc[:,'rawContent']
+        # print(self.df.head(2))
+        self.df['text'] = self.df['rawContent'].apply(self.preprocess_text)
+        self.df['sentiment'] = self.df['text'].apply(self.analyze_sentiment)
 
-# read CSV file into a pandas dataframe
-df = pd.read_csv('training.csv',usecols = ['rawContent'])
-df_test = pd.read_csv('test.csv',usecols = ['rawContent'])
-df_testGroundTruth = pd.read_csv('TweetTest.csv')
+        self.df_test['text'] = self.df_test['rawContent'].apply(self.preprocess_text)
+        self.df_test['sentiment'] = self.df_test['text'].apply(self.analyze_sentiment)
 
-print(df.head(3))
-# df = df.loc[:,'rawContent']
-print(df.head(2))
-df['text'] = df['rawContent'].apply(preprocess_text)
-df['sentiment'] = df['text'].apply(analyze_sentiment)
+        # print the sentiment of each tweet
+        print(self.df['sentiment'])
 
-df_test['text'] = df_test['rawContent'].apply(preprocess_text)
-df_test['sentiment'] = df_test['text'].apply(analyze_sentiment)
+    def eval_accuracy(self):
+        #### Evaluation
+        T = 0
+        for i in range(len(self.df_test)):
+            result = self.sentiment_result(self.df_test.iloc[i, 2])
+            if result == self.df_testGroundTruth.iloc[i, 4]:
+                T += 1
+        print('accuracy = ', round(T / len(self.df_test), 2))
 
-# print the sentiment of each tweet
-print(df['sentiment'])
+    def sentiment_result(self,score):
+        if score > 0:
+            result = 'Positive'
+        elif score < 0:
+            result = 'Negative'
+        else:
+            result = 'Neutral'
+        return result
+
+
+
+
+twitterAnalyzer = TwitterSentimentAnalyzer()
+
 
 # test the sentiment analysis model with a sample tweet
 sample_tweet = 'I do not mind using Python for data analysis!'
-sample_sentiment = analyze_sentiment(preprocess_text(sample_tweet))
+sample_sentiment = twitterAnalyzer.analyze_sentiment(twitterAnalyzer.preprocess_text(sample_tweet))
 print(sample_tweet)
 ### our output: function: sentiment_result
-result = sentiment_result(sample_sentiment)
+result = twitterAnalyzer.sentiment_result(sample_sentiment)
 print('Sentiment:', result)
-#### Evaluation
-T = 0
-for i in range(len(df_test)):
-    result = sentiment_result(df_test.iloc[i, 2])
-    if result == df_testGroundTruth.iloc[i, 4]:
-        T+=1
-print('accuracy = ',round(T/len(df_test),2))
+
 
 print("done")
